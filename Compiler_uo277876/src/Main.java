09681259ecc6870ffabaf05fbaa9e128f010895f
Vantix <79647031+UO277876@users.noreply.github.com>
2023-05-11 03:11:30 +0200

    cambiado a lab11

--- Compiler_uo277876/src/Main.java
+++ Compiler_uo277876/src/Main.java
@@ -1,3 +1,5 @@
+import codegenerator.CodeGenerator;
+import codegenerator.ExecuteCGVisitor;
 import codegenerator.OffSetVisitor;
 import org.antlr.v4.runtime.*;
 import introspector.model.IntrospectorModel;
@@ -12,8 +14,8 @@ import semantic.TypeCheckingVisitor;
 public class Main {
 
 	public static void main(String... args) throws Exception {
-		if (args.length<1) {
-			System.err.println("Please, pass me the input file.");
+		if (args.length<2) {
+			System.err.println("Please, pass me the input and output files.");
 			return;
 		}
 
@@ -37,8 +39,9 @@ public class Main {
 		else{
 			// * The AST is shown
 			ast.accept(new OffSetVisitor(),null);
-			IntrospectorModel model=new IntrospectorModel("Program", ast);
-			new IntrospectorView("Introspector", model);
+			ast.accept(new ExecuteCGVisitor(new CodeGenerator(args[1], args[0])), null);
+			//IntrospectorModel model=new IntrospectorModel("Program", ast);
+			//new IntrospectorView("Introspector", model);
 		}
 	}
 }
